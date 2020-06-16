165a9c7c93d72b38dd2e49b95d1f7d92df73b970
Mitja Galun <mitja@fsresitve.si>
2020-06-16 11:53:15 +0200

    added new Event type for handling non clickable items on grid

--- src/Event/Event.js
+++ src/Event/Event.js
@@ -3,21 +3,40 @@ import PropTypes from 'prop-types';
 import {
   Text,
   TouchableOpacity,
+  TouchableWithoutFeedback,
+  View
 } from 'react-native';
 import styles from './Event.styles';
 
 const Event = ({ event, onPress,onLongPress,style }) => {
+  if (event.isClose) {
+      return (
+          <TouchableWithoutFeedback
+              onLongPress={()=>onLongPress(event)}
+              onPress={() => onPress(event)}
+          >
+              <View
+                  style={[styles.item, style, {
+                      backgroundColor: '#c5575769',
+                      borderRadius: 0
+                  }]}
+              >
+                  <Text style={styles.description}>{event.description}</Text>
+              </View>
+          </TouchableWithoutFeedback>
+      )
+  }
   return (
-    <TouchableOpacity
-      onLongPress={()=>onLongPress(event)}
-      onPress={() => onPress(event)}
-      style={[styles.item, style, {
-        backgroundColor: event.color,
-      }]}
-    >
-      <Text style={styles.description}>{event.description}</Text>
-    </TouchableOpacity>
-  );
+      <TouchableOpacity
+          onLongPress={()=>onLongPress(event)}
+          onPress={() => onPress(event)}
+          style={[styles.item, style, {
+              backgroundColor: event.color,
+          }]}
+      >
+          <Text style={styles.description}>{event.description}</Text>
+      </TouchableOpacity>
+);
 };
 
 const eventPropTypes = PropTypes.shape({
